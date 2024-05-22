4b3f370f89f9f1f0578f8b5c8aab6772073eb536
mansimrat123 <mansimrat66@gmail.com>
2024-05-22 10:42:05 +0530

    Authentication Changes

---
+++ demo/demo/src/main/java/com/example/demo/config/JWTConstants.java
@@ -0,0 +1,6 @@
+package com.example.demo.config;
+
+public class JWTConstants {
+    public static final String SECRET_KEY="jswdnefbhwbfwnenfrjfbghrfbwncdnjnfjwnjd" ;
+    public static final String JWT_HEADER="Authorization" ;
+}
---
+++ demo/demo/src/main/java/com/example/demo/config/JwtTokenValidator.java
@@ -0,0 +1,52 @@
+package com.example.demo.config;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import jakarta.security.auth.message.callback.SecretKeyCallback;
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+import io.jsonwebtoken.security.Keys;
+
+import javax.crypto.SecretKey;
+import java.io.IOException;
+import java.util.List;
+
+public class JwtTokenValidator extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+        String jwt = request.getHeader(JWTConstants.JWT_HEADER) ;
+        if (jwt != null){
+            //Bearer jwt
+            jwt = jwt.substring(7);
+            try{
+                SecretKey key= Keys.hmacShaKeyFor(JWTConstants.SECRET_KEY.getBytes()) ;
+                Claims claims = Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJwt(jwt).getBody();
+
+                String email = String.valueOf(claims.get("email")) ;
+                String authorities = String.valueOf(claims.get("authorities")) ;
+
+                List<GrantedAuthority> auths = AuthorityUtils.commaSeparatedStringToAuthorityList(authorities) ;
+                Authentication authentication =  new UsernamePasswordAuthenticationToken(email , null , auths) ;
+
+                SecurityContextHolder.getContext().setAuthentication(authentication) ;
+
+            }
+catch(Exception e){
+ throw new BadCredentialsException("Invalid token..") ;
+}
+        }
+filterChain.doFilter(request, response ) ;
+    }
+}
